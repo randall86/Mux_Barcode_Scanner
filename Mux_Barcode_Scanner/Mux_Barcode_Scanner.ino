19a9f3812a197a58d587cf17f7f3d6484b4cbb24
Randall Chuah <lynaries@gmail.com>
2018-03-09 15:57:36 +0800

    Added barcode scanner mux selection function

--- Mux_Barcode_Scanner/Mux_Barcode_Scanner.ino
+++ Mux_Barcode_Scanner/Mux_Barcode_Scanner.ino
@@ -2,6 +2,7 @@
 #include <SimpleTimer.h>
 #include <SoftwareSerial.h>
 
+#define MUX_MASK 0x7
 #define MUX_A2 7
 #define MUX_A1 6
 #define MUX_A0 5
@@ -34,7 +35,11 @@
 #define DEBOUNCE_FREQ_MSEC 200 //stable time before switch state changed
 #define TIMER_FREQ_MSEC 50 //read the switch every 50ms
 
-DTIOI2CtoParallelConverter g_ioExpandr(0x74);  //PCA9539 I/O Expander (with A1 = 0 and A0 = 0)
+#define GET_MUX_BIT(A, B) ((A >> (B & MUX_MASK)) & 1)
+#define NOP __asm__("nop\n\t") //"nop" executes in one machine cycle (at 16 MHz) yielding a 62.5 ns delay
+
+//PCA9539 I/O Expander (with A1 = 0 and A0 = 0)
+DTIOI2CtoParallelConverter g_ioExpandr(0x74);
 
 SoftwareSerial g_muxSerial(MUX_RX, MUX_TX); // RX, TX
 
@@ -42,54 +47,90 @@ SoftwareSerial g_muxSerial(MUX_RX, MUX_TX); // RX, TX
 SimpleTimer g_timer;
 
 byte g_debouncedState = 0;
+byte g_isFixtureDetected = 0;
+
+void selectBScanner(byte index)
+{
+  //disable the mux
+  digitalWrite(MUX_EN, LOW);
+  
+  NOP; //60ns delay
+  
+  //update the mux address lines
+  digitalWrite(MUX_A0, GET_MUX_BIT(index, 0));
+  digitalWrite(MUX_A1, GET_MUX_BIT(index, 1));
+  digitalWrite(MUX_A2, GET_MUX_BIT(index, 2));
+  
+  NOP; //60ns delay
+  
+  //enable the mux with the updated address
+  digitalWrite(MUX_EN, HIGH);
+}
+
+void scanBScanner(byte index)
+{
+  //enable barcode scanning
+  g_ioExpandr.digitalWrite0(index, LOW);
+  
+  //get the scanned data
+  if(g_muxSerial.available())
+  {
+    char c = g_muxSerial.read();
+  }
+  
+  //disable barcode scanning
+  g_ioExpandr.digitalWrite0(index, HIGH);
+}
 
 //returns true if state changed
 bool debounceSW(byte *state)
 {
-    static uint8_t count = DEBOUNCE_FREQ_MSEC/TIMER_FREQ_MSEC;
-    bool state_changed = false;
-
-    //read the fixture switch state
-    byte raw_state = digitalRead(FIXTURE_SW);
-    *state = g_debouncedState;
-
-    if (raw_state == g_debouncedState)
+  static uint8_t count = DEBOUNCE_FREQ_MSEC/TIMER_FREQ_MSEC;
+  bool state_changed = false;
+  
+  //read the fixture switch state
+  byte raw_state = digitalRead(FIXTURE_SW);
+  *state = g_debouncedState;
+  
+  if (raw_state == g_debouncedState)
+  {
+    //set the timer which allows a change from current state.
+    count = DEBOUNCE_FREQ_MSEC/TIMER_FREQ_MSEC;
+  }
+  else
+  {
+    //state has changed - wait for new state to become stable.
+    if (--count == 0)
     {
-        //set the timer which allows a change from current state.
-        count = DEBOUNCE_FREQ_MSEC/TIMER_FREQ_MSEC;
+      // Timer expired - accept the change.
+      g_debouncedState = raw_state;
+      state_changed = true;
+      *state = g_debouncedState;
+      
+      // And reset the timer.
+      count = DEBOUNCE_FREQ_MSEC/TIMER_FREQ_MSEC;
     }
-    else
-    {
-        //state has changed - wait for new state to become stable.
-        if (--count == 0)
-        {
-            // Timer expired - accept the change.
-            g_debouncedState = raw_state;
-            state_changed = true;
-            *state = g_debouncedState;
-            
-            // And reset the timer.
-            count = DEBOUNCE_FREQ_MSEC/TIMER_FREQ_MSEC;
-        }
-    }
-
-    return state_changed;
+  }
+  
+  return state_changed;
 }
 
 void debounceSWRoutine()
 {
-    byte sw_state = 0;
-    
-    //if fixture switch state changed, update the state
-    if(debounceSW(&sw_state))
+  byte sw_state = 0;
+  
+  //if fixture switch state changed, update the state
+  if(debounceSW(&sw_state))
+  {
+    if(sw_state)
     {
-        if(sw_state)
-        {
-        }
-        else
-        {
-        }
+      g_isFixtureDetected = 1;
     }
+    else
+    {
+      g_isFixtureDetected = 0;
+    }
+  }
 }
 
 void setup()
@@ -98,27 +139,29 @@ void setup()
   Wire.begin(); //need to start the Wire for I2C devices to function
   
   //set the data rate for the SoftwareSerial port
-  g_muxSerial.begin(9600);
+  g_muxSerial.begin(115200);
 
   //init the barcode scanner enable pins and LED as output
   g_ioExpandr.portMode0(ALLOUTPUT);
   g_ioExpandr.digitalWritePort0(0xFF);
   g_ioExpandr.portMode1(ALLOUTPUT);
-  g_ioExpandr.digitalWritePort1(0xFF);
+  g_ioExpandr.digitalWritePort1(0x00);
 
-  //init the MUX enable pins and address pins as output
+  //init the MUX enable pins
   pinMode(MUX_EN, OUTPUT);
   digitalWrite(MUX_EN, LOW);
+  
+  //init the MUX address pins
   pinMode(MUX_A0, OUTPUT);
-  pinMode(MUX_A1, OUTPUT);
-  pinMode(MUX_A2, OUTPUT);
   digitalWrite(MUX_A0, LOW);
+  pinMode(MUX_A1, OUTPUT);
   digitalWrite(MUX_A1, LOW);
+  pinMode(MUX_A2, OUTPUT);
   digitalWrite(MUX_A2, LOW);
 
   //init the fixture LED output pin and sw detection input pin
   pinMode(FIXTURE_LED, OUTPUT);
-  digitalWrite(FIXTURE_LED, HIGH);
+  digitalWrite(FIXTURE_LED, LOW);
   pinMode(FIXTURE_SW, INPUT);
 
   //initialize the debounce timer
@@ -127,9 +170,19 @@ void setup()
 
 void loop()
 {
-  if(g_muxSerial.available())
+  if(g_isFixtureDetected)
   {
-    char c = g_muxSerial.read();
+    //turn on the fixture detected LED
+    digitalWrite(FIXTURE_LED, HIGH);
+    
+    //loop through all the barcode scanners
+    //for()
+    //{
+    //}
+  }
+  else
+  {
+    //turn off the LED when fixture is removed
+    digitalWrite(FIXTURE_LED, LOW);
   }
 }
-
