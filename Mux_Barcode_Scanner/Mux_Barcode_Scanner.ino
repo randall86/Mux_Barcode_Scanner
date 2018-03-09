6abbaa5e7d4b2fd129ee26b55549a6159b4966e8
randall86 <lynaries@gmail.com>
2018-03-10 00:47:00 +0800

    Completed the coding, pending tests with the actual HW

--- Mux_Barcode_Scanner/Mux_Barcode_Scanner.ino
+++ Mux_Barcode_Scanner/Mux_Barcode_Scanner.ino
@@ -2,7 +2,6 @@
 #include <SimpleTimer.h>
 #include <SoftwareSerial.h>
 
-#define MUX_MASK 0x7
 #define MUX_A2 7
 #define MUX_A1 6
 #define MUX_A0 5
@@ -13,30 +12,34 @@
 #define FIXTURE_SW 9
 #define FIXTURE_LED 8
 
-//expander pins
-#define BSCANNER_EN1 PIN0_0
-#define BSCANNER_EN2 PIN0_1
-#define BSCANNER_EN3 PIN0_2
-#define BSCANNER_EN4 PIN0_3
-#define BSCANNER_EN5 PIN0_4
-#define BSCANNER_EN6 PIN0_5
-#define BSCANNER_EN7 PIN0_6
-#define BSCANNER_EN8 PIN0_7
-
-#define BSCANNER_LED1 PIN1_0
-#define BSCANNER_LED2 PIN1_1
-#define BSCANNER_LED3 PIN1_2
-#define BSCANNER_LED4 PIN1_3
-#define BSCANNER_LED5 PIN1_4
-#define BSCANNER_LED6 PIN1_5
-#define BSCANNER_LED7 PIN1_6
-#define BSCANNER_LED8 PIN1_7
-
+#define SCANNER_DELAY_MSEC 50 //how long to block for reading the scanner data
 #define DEBOUNCE_FREQ_MSEC 200 //stable time before switch state changed
 #define TIMER_FREQ_MSEC 50 //read the switch every 50ms
 
-#define GET_MUX_BIT(A, B) ((A >> (B & MUX_MASK)) & 1)
+#define BSCANNER_MAX_NUM 8
+#define BSCANNER_BIT_MASK ((1 << BSCANNER_MAX_NUM) - 1)
+
+#define GET_BIT(val, shift) ((val >> (shift & BSCANNER_BIT_MASK)) & 1)
 #define NOP __asm__("nop\n\t") //"nop" executes in one machine cycle (at 16 MHz) yielding a 62.5 ns delay
+
+typedef struct _bscanner_param_t
+{
+  byte mux_addr;
+  byte en_pin;
+  byte led_pin;
+}bscanner_param_t;
+
+bscanner_param_t g_bscanner[BSCANNER_MAX_NUM] =
+{
+  {0x0, PIN0_0, PIN1_0},
+  {0x1, PIN0_1, PIN1_1},
+  {0x2, PIN0_2, PIN1_2},
+  {0x3, PIN0_3, PIN1_3},
+  {0x4, PIN0_4, PIN1_4},
+  {0x5, PIN0_5, PIN1_5},
+  {0x6, PIN0_6, PIN1_6},
+  {0x7, PIN0_7, PIN1_7}
+};
 
 //PCA9539 I/O Expander (with A1 = 0 and A0 = 0)
 DTIOI2CtoParallelConverter g_ioExpandr(0x74);
@@ -47,7 +50,8 @@ SoftwareSerial g_muxSerial(MUX_RX, MUX_TX); // RX, TX
 SimpleTimer g_timer;
 
 byte g_debouncedState = 0;
-byte g_isFixtureDetected = 0;
+bool g_isFixtureDetected = false;
+bool g_isFixtureRemoved = false;
 
 void selectBScanner(byte index)
 {
@@ -57,9 +61,9 @@ void selectBScanner(byte index)
   NOP; //60ns delay
   
   //update the mux address lines
-  digitalWrite(MUX_A0, GET_MUX_BIT(index, 0));
-  digitalWrite(MUX_A1, GET_MUX_BIT(index, 1));
-  digitalWrite(MUX_A2, GET_MUX_BIT(index, 2));
+  digitalWrite(MUX_A0, GET_BIT(g_bscanner[index].mux_addr, 0));
+  digitalWrite(MUX_A1, GET_BIT(g_bscanner[index].mux_addr, 1));
+  digitalWrite(MUX_A2, GET_BIT(g_bscanner[index].mux_addr, 2));
   
   NOP; //60ns delay
   
@@ -70,16 +74,24 @@ void selectBScanner(byte index)
 void scanBScanner(byte index)
 {
   //enable barcode scanning
-  g_ioExpandr.digitalWrite0(index, LOW);
+  g_ioExpandr.digitalWrite0(g_bscanner[index].en_pin, LOW);
+
+  //print the barcode scanner index
+  Serial.print(index);
+  Serial.print(":");
   
   //get the scanned data
-  if(g_muxSerial.available())
+  while(g_muxSerial.available())
   {
-    char c = g_muxSerial.read();
+    Serial.write(g_muxSerial.read());
+    delay(SCANNER_DELAY_MSEC); //delay for complete data
   }
+
+  //print LF to indicate end of data
+  Serial.println();
   
   //disable barcode scanning
-  g_ioExpandr.digitalWrite0(index, HIGH);
+  g_ioExpandr.digitalWrite0(g_bscanner[index].en_pin, HIGH);
 }
 
 //returns true if state changed
@@ -124,19 +136,23 @@ void debounceSWRoutine()
   {
     if(sw_state)
     {
-      g_isFixtureDetected = 1;
+      g_isFixtureDetected = true;
     }
     else
     {
-      g_isFixtureDetected = 0;
+      g_isFixtureRemoved = true;
     }
   }
 }
 
 void setup()
 {
-  Serial.begin(115200);
   Wire.begin(); //need to start the Wire for I2C devices to function
+  Serial.begin(115200);
+  
+  // define pin modes for tx, rx
+  pinMode(MUX_RX, INPUT);
+  pinMode(MUX_TX, OUTPUT);
   
   //set the data rate for the SoftwareSerial port
   g_muxSerial.begin(115200);
@@ -170,19 +186,44 @@ void setup()
 
 void loop()
 {
+  //perform scanning if the fixture is in place
   if(g_isFixtureDetected)
   {
-    //turn on the fixture detected LED
+    //toggle the fixture detected LED to green
     digitalWrite(FIXTURE_LED, HIGH);
     
     //loop through all the barcode scanners
-    //for()
-    //{
-    //}
+    for(int index = 0; index < BSCANNER_MAX_NUM; index++)
+    {
+      selectBScanner(index); //select the barcode scanner mux addr
+      scanBScanner(index); //perform the scanning and send the data
+    }
+
+    g_isFixtureDetected = false;
   }
-  else
+
+  //reset the LEDs when the fixture is removed
+  if(g_isFixtureRemoved)
   {
-    //turn off the LED when fixture is removed
+    //toggle the LED to red when fixture is removed
     digitalWrite(FIXTURE_LED, LOW);
+
+    //reset the barcode LED to red
+    g_ioExpandr.digitalWritePort1(0x00);
+
+    g_isFixtureRemoved = false;
+  }
+
+  //process the scanning results
+  if(Serial.available())
+  {
+    byte result = Serial.read();
+    
+    //loop through all the barcode scanners and toggle the LED
+    for(int index = 0; index < BSCANNER_MAX_NUM; index++)
+    {
+      g_ioExpandr.digitalWrite1(g_bscanner[index].led_pin, GET_BIT(result, index));
+    }
   }
 }
+
